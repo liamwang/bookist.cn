d71f94ec072eb4ba7c495b0205277dec9b512712
Liam <liam@live.com>
2021-10-26 19:53:51 +0800

    最小化功能开发，第一个上线版本

--- Bookist.Web/Program.cs
+++ Bookist.Web/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// 注册服务
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+// 注册中间件
+app.UseStaticFiles();
+app.MapRazorPages();
 
 app.Run();
